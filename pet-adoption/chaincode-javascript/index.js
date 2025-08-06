b6cc3c2f7d8ff2ac62c804bd70118c946f9d0ebc
nayatronix01 <61497470+nayatronix01@users.noreply.github.com>
2025-08-06 07:19:00 +0100

    Update index.js
    Signed-off-by: nayatronix01 <61497470+nayatronix01@users.noreply.github.com>

--- pet-adoption/chaincode-javascript/index.js
+++ pet-adoption/chaincode-javascript/index.js
@@ -1,14 +1,3 @@
 'use strict';
-
-console.log('[CHAINCODE] index.js loading...');
-
-const { Shim } = require('fabric-shim');
-const AdoptionContract = require('./adoption-contract');
-
-try {
-    console.log('[CHAINCODE] Starting chaincode...');
-    Shim.start(new AdoptionContract());
-} catch (e) {
-    console.error('[CHAINCODE] Chaincode start failed:', e);
-    throw e;
-}
+const PetShop = require('./petshop');
+module.exports.contracts = [PetShop];
